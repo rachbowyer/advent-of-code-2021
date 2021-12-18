0e3c1263a024c66966e7c7e5a8278580149ae1ce
Rachel Bowyer <rachbowyer@gmail.com>
2021-12-18 18:02:10 +0000

    Day 15

---
+++ src/advent_of_code/day15.clj
@@ -0,0 +1,91 @@
+(ns advent-of-code.day15
+  (:require [clojure.data.priority-map :refer [priority-map]]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn- parse-file [file]
+  (->> file io/resource slurp str/split-lines
+       (mapv (fn [e] (mapv #(Integer/parseInt (str %)) e)))))
+
+(def ^:private expansion-constant 5)
+
+(def ^:private moves [[0 1] [0 -1] [1 0] [-1 0]])
+
+(defn size-x [grid]
+  (-> grid first count))
+
+(defn size-y [grid]
+  (count grid))
+
+(defn get-value [grid [x y]]
+  (get (get grid y) x))
+
+(def start-point [0 0])
+
+(defn end-point [grid]
+  [(-> grid size-x dec) (-> grid size-y dec)])
+
+(defn- expand [direction block]
+  (loop [i          1
+         last-block block
+         result     block]
+    (if (= i expansion-constant)
+      (vec result)
+      (let [f (fn [r] (mapv #(if (= % 9) 1 (inc %)) r))
+            new-block (if (= direction :across)
+                        (f last-block)
+                        (map f last-block))]
+        (recur (inc i) new-block (concat result new-block))))))
+
+(defn- expand-grid [grid]
+  (->> grid (mapv (partial expand :across)) (expand :down )))
+
+(defn- heuristic
+
+  [[x1 y1] [x2 y2]]
+  (+ (Math/abs (- x2 x1)) (Math/abs (- y2 y1))))
+
+(defn- get-successors [grid [x y]]
+  (->> moves
+       (map (fn [[x' y']] [(+ x x') (+ y y')]))
+       (remove (fn [[x y]] (or (neg? x) (neg? y)
+               (>= x (size-x grid)) (>= y (size-y grid)))))))
+
+(defn- get-lowest-risk
+  [grid start-point end-point]
+  (loop [explored #{start-point}
+         priority-queue (priority-map [start-point 0] ; distance from start point
+                                      (heuristic start-point end-point))]
+    (let [[[node distance] _] (peek priority-queue)]
+      (if (= node end-point)
+        distance
+        (let [new-queue
+              (->> (get-successors grid node)
+                   (remove explored)
+                   (map (fn [s]
+                          (let [distance-to-node (+ distance (get-value grid s))]
+                            [[s distance-to-node]
+                             (+ distance-to-node (heuristic s end-point))])))
+                   (reduce conj (pop priority-queue)))]
+          (recur (conj explored node) new-queue))))))
+
+(defn- get-lowest-risk-start-end [grid]
+  (get-lowest-risk grid start-point (end-point grid)))
+
+(defn day15-solution-part1 []
+  (-> "input.day15.txt" parse-file get-lowest-risk-start-end))
+
+(defn day15-solution-part2 []
+  (-> "input.day15.txt" parse-file expand-grid get-lowest-risk-start-end))
+
+;; My original solution used dynamic programming. However, this only works
+;; if the optimal path is always either down or right; this is not the case.
+;;
+;; New and correct solution uses A* search.
+;;
+;; The Heuristic is based on Manhattan distance
+;; 1. It is admissible. This means that A* finds the minimum distance
+;; 2. It is complete. This means that first time it sees a node it has found the
+;;    shortest distance to the node. This avoids needing to update distances
+;;    to successors later."
+
---
+++ test/advent_of_code/day15_test.clj
@@ -0,0 +1,35 @@
+(ns advent-of-code.day15-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.day15 :as d15]))
+
+(def example-grid
+  [[1 1 6 3 7 5 1 7 4 2]
+   [1 3 8 1 3 7 3 6 7 2]
+   [2 1 3 6 5 1 1 3 2 8]
+   [3 6 9 4 9 3 1 5 6 9]
+   [7 4 6 3 4 1 7 1 1 1]
+   [1 3 1 9 1 2 8 1 3 7]
+   [1 3 5 9 9 1 2 4 2 1]
+   [3 1 2 5 4 2 1 6 3 9]
+   [1 2 9 3 1 3 8 5 2 1]
+   [2 3 1 1 9 4 4 5 8 1]])
+
+(deftest end-point-test
+  (is (= [2 2] (#'d15/end-point [[1 2 3] [4 5 6] [7 8 9]]))))
+
+(deftest heuristic-test
+  (are [a b expected] (= expected (#'d15/heuristic a b))
+    [0 0] [3 4] 7
+    [3 4] [0 0] 7
+    [1 2] [2 1] 2))
+
+(deftest get-successors-test
+  (are [point expected]
+    (= expected (#'d15/get-successors [[1 2 3] [4 5 6] [7 8 9]] point))
+    [0 0] [[0 1] [1 0]]
+    [1 1] [[1 2] [1 0] [2 1] [0 1]]
+    [2 2] [[2 1] [1 2]]))
+
+(deftest get-lowest-risk-start-end-test
+  (is (= 40 (#'d15/get-lowest-risk-start-end example-grid)))
+  (is (= 315 (#'d15/get-lowest-risk-start-end (#'d15/expand-grid example-grid)))))
